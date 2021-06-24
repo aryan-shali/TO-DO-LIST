48de83c87f1faf790f3fe73c197f0f9418cefd7d
aryan <shaliaryan75@gmail.com>
2021-06-24 12:26:25 +0430

    make the add button

--- app.js
+++ app.js
@@ -13,3 +13,37 @@ for (var i = 0; i < cbut.length; i++) {
         parent.remove();
     }
 }
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('check-ele');
+    }
+}, false);
+
+function addb() {
+    var inp = document.getElementById('input').value;
+
+    var newitem = document.createElement('li');
+    var newtxt = document.createTextNode(inp)
+    newitem.appendChild(newtxt);
+    if (inp === '') {
+        alert("write something")
+    } else {
+        document.getElementById('uu').appendChild(newitem);
+    }
+    document.getElementById('input').value = " ";
+    newitem.id = "ll";
+    var span = document.createElement("SPAN");
+    var text = document.createTextNode("\u00D7");
+    span.className = "cbut";
+    span.appendChild(text);
+    list.appendChild(span);
+
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+
+        }
+    }
+}
